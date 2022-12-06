a8bc33351e9146feb066238ebf5176bccc9476eb
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-12-06 15:43:04 -0500

    advent of code day 6 part 1

---
+++ day6/day6.py
@@ -0,0 +1,17 @@
+with open('input.txt') as f:
+    string = f.read()
+def day_6(x):
+    for i in range(len(string)):
+        if i >= x-1:
+            temp = string[i:i-x:-1]
+            unique = []
+            for j in temp:
+                if j not in unique:
+                    unique.append(j)
+                else:
+                    pass
+            if len(unique) == x:
+                print(i+1)
+                break
+#part one
+day_6(4)
